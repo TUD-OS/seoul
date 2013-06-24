a43a293db59ebd65bc1d08ba8afa5646e3efed32
Nils Asmussen <nils@os.inf.tu-dresden.de>
2013-06-24 11:15:47 +0200

    Made profiling stuff 64-bit ready (fixes #23)

--- include/service/profile.h
+++ include/service/profile.h
@@ -22,8 +22,33 @@
 // you can enable -DPROFILE to get these counters.
 
 #ifdef PROFILE
-# define PVAR  ".long"
-# define COUNTER_INC(NAME)						\
+# ifdef __x86_64__
+
+#  define PVAR  ".quad"
+// on x86_64, for code optimisation purposes, the default immediate size to the mov instructions
+// is a 32-bit value. this is even true for inc. thus, we use movabs and increment it indirectly.
+#  define COUNTER_INC(NAME)                     \
+  ({                                    \
+    asm volatile (".section .data; 1: .string \"" NAME "\";.previous;"  \
+        ".section .profile; " PVAR " 1b; 2: " PVAR " 0,0;.previous;" \
+        "movabs 2b, %%rax;"               \
+        "incq %%rax;"             \
+        "movabs %%rax, 2b;" : : : "cc", "rax");             \
+  })
+
+
+#  define COUNTER_SET(NAME, VALUE)                  \
+  {                                 \
+    asm volatile (".section .data; 1: .string \"" NAME "\";.previous;"  \
+        ".section .profile; "  PVAR " 1b; 2: " PVAR " 0,0;.previous;" \
+        "movq %0, %%rax;"                 \
+        "movabs %%rax, 2b;" : : "r"(static_cast<long>(VALUE)) : "rax"); \
+  }
+
+# else
+
+#  define PVAR  ".long"
+#  define COUNTER_INC(NAME)						\
   ({									\
     asm volatile (".section .data; 1: .string \"" NAME "\";.previous;"	\
                   ".section .profile; " PVAR " 1b; 2: " PVAR " 0,0;.previous;" \
@@ -31,12 +56,14 @@
   })
 
 
-# define COUNTER_SET(NAME, VALUE)					\
+#  define COUNTER_SET(NAME, VALUE)					\
   {									\
     asm volatile (".section .data; 1: .string \"" NAME "\";.previous;"	\
 		  ".section .profile; "  PVAR " 1b; 2: " PVAR " 0,0;.previous;" \
 		  "mov %0,2b" : : "r"(static_cast<long>(VALUE)));	\
   }
+
+# endif
 
 #else
 # define COUNTER_INC(NAME)        do {} while (0)
