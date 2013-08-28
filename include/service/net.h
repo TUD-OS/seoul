c6fe74c5fbc004eae918417efe9c2db0ef68c769
Nils Asmussen <nils@os.inf.tu-dresden.de>
2013-08-28 11:49:29 +0200

    Fixed type-error in net.h (unsigned vs. size_t).

--- include/service/net.h
+++ include/service/net.h
@@ -254,7 +254,7 @@ public:
 
   // Move data and update TCP/IP checksum.
   static void
-  move(uint8 * dst, uint8 const * src, unsigned size, uint32 &state, bool &odd)
+  move(uint8 * dst, uint8 const * src, size_t size, uint32 &state, bool &odd)
   {
     // Logging::printf("move(%p, %p, %u, %08x, %u)\n", dst, src, size, state, odd);
     // hexdump(src, size);
