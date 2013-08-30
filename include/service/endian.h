4ee9e2d0db2796852463e592d5020663505d6625
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2013-08-30 14:00:20 +0200

    Use builtins for bswap. Enable bswap on x86-64.

--- include/service/endian.h
+++ include/service/endian.h
@@ -23,15 +23,12 @@
 
 namespace Endian {
 
-#ifdef __i386
-  static inline uint16 hton16(uint16 value) { asm ("xchg %b0, %h0" : "+Q"(value)); return value; }
-  static inline uint16 ntoh16(uint16 value) { asm ("xchg %b0, %h0" : "+Q"(value)); return value; }
-  static inline uint32 hton32(uint32 value) { asm ("bswap %0" : "+r"(value)); return value; }
-  static inline uint32 ntoh32(uint32 value) { asm ("bswap %0" : "+r"(value)); return value; }
-
-  static inline uint64 hton64(uint64 value) {
-    return static_cast<uint64>(hton32(value))<<32 | hton32(value>>32);
-  }
+#if defined(__i386__) || defined(__x86_64__)
+  static inline uint16 hton16(uint16 value) { return __builtin_bswap16(value); }
+  static inline uint16 ntoh16(uint16 value) { return __builtin_bswap16(value); }
+  static inline uint32 hton32(uint32 value) { return __builtin_bswap32(value); }
+  static inline uint32 ntoh32(uint32 value) { return __builtin_bswap32(value); }
+  static inline uint64 hton64(uint64 value) { return __builtin_bswap64(value); }
 #else
   #error Port me!
 #endif
